f5a59f337f4dbc1a356f6a45ef4202635a7d86a4
黄宇辉 <Gentleman_0109@outlook.com>
2019-08-26 08:44:23 +0800

    Revert ":tada: refactor the master branch successfully"

--- memo/src/main/java/pers/haungyuhui/memo/MemoApplication.java
+++ memo/src/main/java/pers/haungyuhui/memo/MemoApplication.java
@@ -1,13 +1,16 @@
-package pers.huangyuhui.memo;
+package pers.haungyuhui.memo;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("pers.huangyuhui.memo.dao") //扫描Mapper接口
+@MapperScan("pers.haungyuhui.memo.dao") //扫描Mapper接口
 public class MemoApplication {
 
-    public static void main(String[] args) { SpringApplication.run(MemoApplication.class, args); }
+    public static void main(String[] args) {
+
+        SpringApplication.run(MemoApplication.class, args);
+    }
 
 }
